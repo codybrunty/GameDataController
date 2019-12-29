214a727c726528b5fa7e0e4cfd688594604c2cd2
cody brunty <codybrunty@gmail.com>
2019-12-28 23:34:21 -0800

    Cleaning up GameDataContoller
    Removed some functions used for my game that are not necessary for portfolio piece.

--- GameDataControler.cs
+++ GameDataControler.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
@@ -9,6 +8,7 @@ using UnityEngine.SceneManagement;
 public class GameDataControl : MonoBehaviour {
 
     public static GameDataControl gdControl;
+    
     public int coinsEarned;
     public int coinsSpent;
     public int coinsTotal;
@@ -21,82 +21,31 @@ public class GameDataControl : MonoBehaviour {
     public List<int> daily_hardLevel_Indexes;
     public int profile_puzzlesSolved;
 
-    private bool resetLoaded = false;
-
     private void Awake() {
-        //Run this on first time ever opening game.
-        int firstTime = PlayerPrefs.GetInt("firstTime", 0);
-        if (firstTime == 0) {
-            PlayerPrefs.SetInt("firstTime", 20);
+        //Check if game has been opened before
+        bool firstTimeOpeningGame = PlayerPrefs.GetBool("firstTimeOpeningGame", true);
+        
+        if (firstTimeOpeningGame) {
+            PlayerPrefs.SetBool("firstTimeOpeningGame", false);
             Debug.Log("First Time Opening Game!");
-            FindObjectOfType<SplashScreenMechanics>().firstTime = true;
             ResetPlayerData();
-            resetLoaded = true;
         }
-
+        
+        //Singelton pattern
         if (gdControl == null) {
             DontDestroyOnLoad(gameObject);
             gdControl = this;
-            if (!resetLoaded) {
-                LoadPlayerData();
-            }
+            LoadPlayerData();
         }
-        else if (gdControl != this) {
+        else{
             Destroy(gameObject);
         }
     }
 
-    private void Start() {
-        //PlayerPrefs.DeleteAll();
-        //Debug.Log(Application.persistentDataPath);
-    }
-
-    private void PrintAllLevelResults() {
-        int blocksIndex = gdControl.all_level_results.Count;
-        for (int i = 0; i < blocksIndex; i++) {
-            string lvlResultsString = "";
-            for (int x = 0; x < all_level_results[i].Count; x++) {
-                lvlResultsString += all_level_results[i][x].ToString();
-            }
-            Debug.Log("Block " + i + " All Level Results " + lvlResultsString);
-        }
-    }
-
-    public List<int> GetFullSecretsUnlocked(){
-        List<int> SecretsUnlocked = new List<int> { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-
-        int blocksIndex = gdControl.blockSecretsUnlocked.Count;
-        for (int i = 0; i < blocksIndex; i++){
-            int firstSecret = gdControl.blockSecretsUnlocked[i][0];
-            int secondSecret = gdControl.blockSecretsUnlocked[i][1];
-            int thirdSecret = gdControl.blockSecretsUnlocked[i][2];
-            int fourthSecret = gdControl.blockSecretsUnlocked[i][3];
-            if (firstSecret == 1 && secondSecret == 1 && thirdSecret == 1 && fourthSecret == 1){
-                SecretsUnlocked[i] = 1;
-            }
-        }
-
-        return SecretsUnlocked;
-    }
-
-    public void PrintSecretResults() {
-        int blocksIndex = gdControl.blockSecretsUnlocked.Count;
-        for (int i = 0; i < blocksIndex; i++) {
-            int firstSecret = gdControl.blockSecretsUnlocked[i][0];
-            int secondSecret = gdControl.blockSecretsUnlocked[i][1];
-            int thirdSecret = gdControl.blockSecretsUnlocked[i][2];
-            int fourthSecret = gdControl.blockSecretsUnlocked[i][3];
-            int firstLevel = gdControl.blockSecretsUnlocked[i][4];
-            int secondLevel = gdControl.blockSecretsUnlocked[i][5];
-            int thirdLevel = gdControl.blockSecretsUnlocked[i][6];
-            int fourthLevel = gdControl.blockSecretsUnlocked[i][7];
-            Debug.Log("Block " + i + " Secret Results " + firstSecret + " " + secondSecret + " " + thirdSecret + " " + fourthSecret+" "+ firstLevel + " " + secondLevel + " " + thirdLevel + " " + fourthLevel);
-        }
-    }
-
     public void SavePlayerData() {
+        //Write GameData to a binary file
         BinaryFormatter bf = new BinaryFormatter();
-        FileStream file = File.Create(Application.persistentDataPath + "/bluePathPlayerData2.dat");
+        FileStream file = File.Create(Application.persistentDataPath + "/game.dat");
 
         PlayerData data = new PlayerData();
 
@@ -119,13 +68,13 @@ public class GameDataControl : MonoBehaviour {
     }
 
     public void LoadPlayerData() {
-        if (File.Exists(Application.persistentDataPath + "/bluePathPlayerData2.dat")) {
+        //If GameData exists read it into the controller
+        if (File.Exists(Application.persistentDataPath + "/game.dat")) {
             BinaryFormatter bf = new BinaryFormatter();
-            FileStream file = File.Open(Application.persistentDataPath + "/bluePathPlayerData2.dat", FileMode.Open);
+            FileStream file = File.Open(Application.persistentDataPath + "/game.dat", FileMode.Open);
             PlayerData data = (PlayerData)bf.Deserialize(file);
             file.Close();
-
-
+            
             coinsEarned = data.coinsEarned;
             coinsSpent = data.coinsSpent;
             coinsTotal = data.coinsTotal;
@@ -137,22 +86,22 @@ public class GameDataControl : MonoBehaviour {
             daily_easyLevel_Indexes = data.daily_easyLevel_Indexes;
             daily_hardLevel_Indexes = data.daily_hardLevel_Indexes;
             profile_puzzlesSolved = data.profile_puzzlesSolved;
+            
             Debug.Log("Loaded Player Data from Local File");
-
         }
+        //If GameData doesn't exist Reset Player Data and reload scene
         else {
             Debug.Log("No Player Data found re-creating Player Data");
             ResetPlayerData();
+            //Reload scene after resetting player data
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-            //PrintAllLevelResults();
-            //PrintSecretResults();
         }
     }
 
     public void ResetPlayerData() {
+        //Write new empty GameData to a binary file
         BinaryFormatter bf = new BinaryFormatter();
-        FileStream file = File.Create(Application.persistentDataPath + "/bluePathPlayerData2.dat");
-        //Debug.Log(Application.persistentDataPath);
+        FileStream file = File.Create(Application.persistentDataPath + "/game.dat");
         PlayerData data = new PlayerData();
 
         data.coinsEarned = 0;
@@ -191,17 +140,7 @@ public class GameDataControl : MonoBehaviour {
         file.Close();
 
         Debug.Log("Reset Player Data in Local File");
-
         LoadPlayerData();
-    }
-
-    public void AddCoins(int ammount) {
-        coinsEarned += ammount;
-        coinsTotal += ammount;
-    }
-
-    public void AddPuzzleSolved() {
-        profile_puzzlesSolved++;
     }
 
 }
